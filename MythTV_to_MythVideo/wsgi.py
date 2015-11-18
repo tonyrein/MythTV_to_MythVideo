09bdd3dbb2397fe182183302c5610b6e409044be
tonyrein <trein@cinci.rr.com>
2015-11-17 20:13:02 -0500

    Initial commit

---
+++ MythTV_to_MythVideo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for MythTV_to_MythVideo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MythTV_to_MythVideo.settings")
+
+application = get_wsgi_application()
