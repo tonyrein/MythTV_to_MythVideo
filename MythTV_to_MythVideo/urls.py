7b418c6678c9975a2e8e964ef79a5a16ecd7605d
tonyrein <trein@cinci.rr.com>
2015-11-20 21:58:39 -0500

    Work on templates, views, urls

--- MythTV_to_MythVideo/urls.py
+++ MythTV_to_MythVideo/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^mythcontent/', include('mythcontent.urls')),
+    url(r'^mythcontent/', include('mythcontent.urls', namespace='mythcontent')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- mythcontent/views.py
+++ mythcontent/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 from .mythapi import MythApi
 # Create your views here.
@@ -12,12 +13,19 @@ def index(request):
     tv_program_list = api.get_myth_tv_program_list()
     # save list in session:
     request.session['tv_program_list'] = tv_program_list
-    context = { 'tv_program_list': tv_program_list, 'session': request.session }
-    return render(request, 'mythcontent/index.html', context)
+    return render(request, 'mythcontent/index.html')
 
 def videos(request):
     api = MythApi()
     video_list = api.get_mythvideo_list()
     context = {'video_list': video_list}
     return render(request, 'mythcontent/videos.html', context)
-#     return HttpResponse("This page will display a list of videos.")
+
+def process_moves(request):
+    if request.method != 'POST':
+        raise Exception("Must use Post with this view")
+    checkboxlist = request.POST.getlist('selected_programs')
+    template = loader.get_template('mythcontent/process_moves.html')
+    context = RequestContext(request, {'selected_programs': checkboxlist, })
+    return HttpResponse(template.render(context))
+#    return render(request, 'mythcontent/process_moves.html')
