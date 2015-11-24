c922690868f2288e7ab840295b45b4edc0107047
Tony Rein <trein@cinci.rr.com>
2015-11-23 21:08:23 -0500

    Laying out main DTO classes

--- mythcontent/dto.py
+++ mythcontent/dto.py
@@ -1,15 +1,13 @@
 
-
-from mythcontent.mythapi import MythApi
+from mythcontent.data_access import VideoApi, VideoDao, TvRecordingApi,  StorageGroupApi
 
 class TvRecording(object):
     def __init__(self, data):
-        self._data = data # data should be a tv_recording named_tuple
-        st_dir = MythApi().get_storage_dir(name=data.storage_group)
-        self.filespec = st_dir + data.file_name # st_dir already ends with os.sep.
+        pass
             
              
-    
+class StorageGroup(object):
+    pass    
         
 
 class Video(object):
